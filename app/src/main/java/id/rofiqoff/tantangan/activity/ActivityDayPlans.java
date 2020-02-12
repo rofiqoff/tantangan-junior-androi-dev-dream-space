f9f0e4e109618fac606d2c04d6dc22007522221a
rofiqoff <rofiqof@gmail.com>
2020-02-12 15:53:42 +0700

    create new activity

---
+++ app/src/main/java/id/rofiqoff/tantangan/activity/ActivityDayPlans.java
@@ -0,0 +1,16 @@
+package id.rofiqoff.tantangan.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.rofiqoff.tantangan.R;
+
+public class ActivityDayPlans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_plans);
+    }
+}
